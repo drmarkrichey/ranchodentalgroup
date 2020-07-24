ae4bb83a201cab11c34f48810c64075f9cb85238
drmarkrichey <68612607+drmarkrichey@users.noreply.github.com>
2020-07-23 17:23:01 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Handler when the DOM is fully loaded
-  alert("Hello there, our website is underconstruction but feel free to explore and see our services.")
+  alert("Hello there, our website is underconstruction but feel free to explore and check out our services. If you need emergency assistance, you can reach us at (619) 297-6104")
 });
